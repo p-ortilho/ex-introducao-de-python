a784159d4b50bdd1bce9289df2ae77066d275c92
p-ortilho <cainportilho@gmail.com>
2025-03-04 23:11:17 -0300

    feat: Adiciona script para cálculo de imposto e média escolar

---
+++ 04-03-25.py
@@ -0,0 +1,38 @@
+salario = 3000
+
+if salario > 1200:
+    print("Você tem que pagar imposto")
+else:
+    print("Você não tem que pagar imposto")
+
+a = 1
+b = 2
+c = True
+d = False
+
+print(a > b and c or d)
+
+a = 10
+b = 3
+c = False  
+d = False
+
+print(a > b and c or d)
+
+a = 5
+b = 1
+c = True  
+d = True
+
+print(a > b and c or d)
+
+materia1 = 8
+materia2 = 7
+materia3 = 6
+
+media = (materia1 + materia2 + materia3) / 3
+
+if media >= 7:
+    print("Aprovado")
+else:
+    print("Reprovado")
